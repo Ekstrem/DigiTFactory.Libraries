3284d340445581778870d3077dc92f469d3f1949
Evgenei Voitov <about47@yandex.ru>
2019-12-07 10:04:18 +0300

    Update IComplexKey.cs
    is: #6

--- Source/Characteristics/IComplexKey.cs
+++ Source/Characteristics/IComplexKey.cs
@@ -2,11 +2,19 @@ using System;
 
 namespace Hive.SeedWorks.Characteristics
 {
-    public interface IComplexKey : IComplexKey<Guid> { }
+    /// <summary>
+    /// Комплексный ключ агрегата с рекомендуемыми типами данных.
+    /// </summary>
+    public interface IComplexKey : IComplexKey<Guid, long>, IHasKey, IHasVersion { }
 
-    public interface IComplexKey<out TKey> :
+    /// <summary>
+    /// Комплексный ключ агрегата.
+    /// </summary>
+    /// <typeparam name="TKey">Тип идентификатора агрегата.</typeparam>
+    /// <typeparam name="TVersion">Тип идентификатора версии.</typeparam>
+    public interface IComplexKey<out TKey, out TVersion> :
         IHasKey<TKey>,
-        IVersion
+        IHasVersion<TVersion>
     {
     }
 }
