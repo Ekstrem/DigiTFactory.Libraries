e6f36abca97452a4d4a60717da3ecfdbd45ea00b
Evgenei Voitov <about47@yandex.ru>
2019-12-13 17:53:02 +0300

    Update IAnemicModel.cs

--- Source/TacticalPatterns/IAnemicModel.cs
+++ Source/TacticalPatterns/IAnemicModel.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace Hive.SeedWorks.TacticalPatterns
 {
     /// <summary>
     /// Анемичная модель ограниченного контекста для Фабрики создания агрегата.
+    /// Анемичная модель должна содержать объект-значения, реализующие интерфейс <see cref="IValueObject"/>.
     /// </summary>
-    public interface IAnemicModel<TBoundedContext>
+    public interface IAnemicModel<TBoundedContext> :
+        IAggregateRoot<TBoundedContext>
         where TBoundedContext : IBoundedContext
     {
-        /// <summary>
-        /// Корень модели сущности.
-        /// </summary>
-        IAggregateRoot<TBoundedContext> Root { get; }
-
         /// <summary>
         /// Словарь объект значений.
         /// </summary>
