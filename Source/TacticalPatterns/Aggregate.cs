c6d4c437051ba2727311783d241b08eb9bfc9b8e
Evgenei Voitov <about47@yandex.ru>
2020-01-10 13:26:40 +0300

    Update Aggregate.cs

--- Source/TacticalPatterns/Aggregate.cs
+++ Source/TacticalPatterns/Aggregate.cs
@@ -12,6 +12,8 @@ namespace Hive.SeedWorks.TacticalPatterns
         private readonly IComplexKey _key;
         private readonly IAnemicModel<TBoundedContext> _anemicModel;
         private readonly IBoundedContextScope<TBoundedContext> _scope;
+        private string _commandName;
+        private string _subjectName;
 
         private Aggregate(
             IComplexKey key,
@@ -46,7 +48,7 @@ namespace Hive.SeedWorks.TacticalPatterns
         /// <summary>
         /// Словарь объект значений.
         /// </summary>
-        public IDictionary<string, IValueObject> ValueObjects => _anemicModel.ValueObjects;
+        public IDictionary<string, IValueObject> Invariants => _anemicModel.Invariants;
 
         /// <summary>
         /// Бизнес-операции - фабрики.
@@ -56,7 +58,17 @@ namespace Hive.SeedWorks.TacticalPatterns
         /// <summary>
         /// Валидаторы модели бизнес-объекта.
         /// </summary>
-        public IReadOnlyList<IBusinessValidator<TBoundedContext>> Validators => _scope.Validators;
+        public IReadOnlyList<IBusinessEntityValidator<TBoundedContext>> Validators => _scope.Validators;
+
+        /// <summary>
+        /// Имя метода агрегата, который вызывает команда.
+        /// </summary>
+        public string CommandName => _commandName;
+
+        /// <summary>
+        /// Имя субъекта бизнес-операции.
+        /// </summary>
+        public string SubjectName => _subjectName;
 
         public static IAggregate<TBoundedContext> CreateInstance(
             IComplexKey key,
