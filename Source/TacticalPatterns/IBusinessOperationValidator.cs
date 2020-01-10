9576e9005b7b29f7d9ab7fc4a65f76da0f58aa50
Evgenei Voitov <about47@yandex.ru>
2020-01-10 16:31:40 +0300

    Update IBusinessOperationValidator.cs
    is: #5

--- Source/TacticalPatterns/IBusinessOperationValidator.cs
+++ Source/TacticalPatterns/IBusinessOperationValidator.cs
@@ -1,9 +1,16 @@
 namespace Hive.SeedWorks.TacticalPatterns
 {
-    public interface IBusinessOperationValidator<TOperation, TBoundedContext>
+    public interface IBusinessOperationValidator<TBoundedContext> :
+        IValidator<TBoundedContext>
+        where TBoundedContext : IBoundedContext
+    {
+        string Name { get; }
+    }
+
+    public interface IBusinessOperationValidator<TOperation, TBoundedContext>:
+        IBusinessOperationValidator<TBoundedContext>
         where TOperation : IAggregateBusinessOperation<TBoundedContext>
         where TBoundedContext : IBoundedContext
     {
-        bool ValidateModel(IAnemicModel<TBoundedContext> anemicModel);
     }
 }
