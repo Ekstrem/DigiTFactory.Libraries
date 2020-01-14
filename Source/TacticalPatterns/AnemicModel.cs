ab82e4c4a35fd13920d1e1fcbf532769e5daa0bb
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-01-14 13:04:50 +0400

    update AnemicModel to make it work with EF

--- Source/TacticalPatterns/AnemicModel.cs
+++ Source/TacticalPatterns/AnemicModel.cs
@@ -1,10 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using Hive.SeedWorks.Characteristics;
-using Hive.SeedWorks.Monads;
 
 namespace Hive.SeedWorks.TacticalPatterns
 {
@@ -12,79 +7,60 @@ namespace Hive.SeedWorks.TacticalPatterns
     /// Базовый класс анемичной модели.
     /// </summary>
     /// <typeparam name="TBoundedContext">Ограниченный контекст.</typeparam>
-    public abstract class AnemicModel<TBoundedContext> : 
+    public abstract class AnemicModel<TBoundedContext> :
         IAnemicModel<TBoundedContext>
         where TBoundedContext : IBoundedContext
     {
-        private readonly IComplexKey _complexKey;
-        private readonly IDictionary<string, IValueObject> _invariants;
+		private Guid _id;
+		protected long _version;
+		private Guid _correlationToken;
+		private IDictionary<string, IValueObject> _invariants;
 
-        /// <summary>
-        /// Конструктор анемичной модели.
-        /// Потомки должны реализовать свой конструктор с валидацией объект значений.
-        /// </summary>
-        /// <param name="complexKey">Составной ключ.</param>
-        /// <param name="valueObjects">Словарь объект значений.</param>
-        public AnemicModel(
-            IComplexKey complexKey,
-            IDictionary<string, IValueObject> invariants)
-        {
-            _complexKey = complexKey;
-            // validating Aggregate Root.
-            if (!invariants.ContainsKey("Root"))
-            {
-                throw new ArgumentException("Root must be Root.");
-            }
+		protected AnemicModel()
+		{
+		}
 
-            _invariants = invariants;
-        }
-
-        /// <summary>
-        /// Конструктор анемичной модели.
-        /// Потомки должны реализовать свой конструктор с валидацией объект значений.
-        /// </summary>
-        /// <param name="root">Корень агрегата.</param>
-        /// <param name="invariants">Словарь объект значений.</param>
-        public AnemicModel(
-            IAggregateRoot<TBoundedContext> root,
-            IDictionary<string, IValueObject> invariants)
-            : this((IComplexKey)root, invariants.Do(vo => vo.Add("Root", root)))
-        {
-        }
-
-        /// <summary>
-        /// Конструктор анемичной модели.
-        /// Потомки должны реализовать свой конструктор с валидацией объект значений.
-        /// </summary>
-        /// <param name="complexKey">Составной ключ.</param>
-        /// <param name="invariants">Словарь объект значений.</param>
-        public AnemicModel(
-            IComplexKey complexKey,
-            params IValueObject[] invariants)
-            : this(complexKey, invariants.ToImmutableDictionary(k => k.GetType().Name, v => v))
-        {
-        }
+		public AnemicModel(
+			Guid id,
+			long version,
+			Guid correlationToken)
+		{
+			_id = id;
+			_version = version;
+			_correlationToken = correlationToken;
+		}
 
         /// <summary>
         /// Имеет идентификатор.
         /// </summary>
-        public Guid Id => _complexKey.Id;
+        public Guid Id => _id;
 
         /// <summary>
         /// Определяет версию. Ожидаемое использование - дата создания версии в милисекундах.
         /// Является приведением <see cref="DateTimeOffset"/> к формату времени
         /// Unix в милисекундах.
         /// </summary>
-        public long Version => _complexKey.Version;
+        public long Version => _version;
 
         /// <summary>
         /// Идентификатор комманды, создавшей новую версию.
         /// </summary>
-        public Guid CorrelationToken => _complexKey.CorrelationToken;
+        public Guid CorrelationToken => _correlationToken;
 
         /// <summary>
         /// Словарь объект значений.
         /// </summary>
-        public IDictionary<string, IValueObject> Invariants => _invariants;
-    }
+        public IDictionary<string, IValueObject> Invariants => GetInvariants();
+
+		private IDictionary<string, IValueObject> GetInvariants()
+		{
+			if (_invariants != null)
+			{
+				return _invariants;
+			}
+
+			_invariants = this.GetFields();
+			return _invariants;
+		}
+	}
 }
