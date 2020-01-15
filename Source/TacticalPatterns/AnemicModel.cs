66a5f96e8c8f9137c41843d4a7f6a12ed3dbdc0a
Semyon Kirnosenko <semyon.kirnosenko@gmail.com>
2020-01-15 11:18:55 +0400

    formatting fix

--- Source/TacticalPatterns/AnemicModel.cs
+++ Source/TacticalPatterns/AnemicModel.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 
 namespace Hive.SeedWorks.TacticalPatterns
 {
-    /// <summary>
-    /// Базовый класс анемичной модели.
-    /// </summary>
-    /// <typeparam name="TBoundedContext">Ограниченный контекст.</typeparam>
-    public abstract class AnemicModel<TBoundedContext> :
-        IAnemicModel<TBoundedContext>
-        where TBoundedContext : IBoundedContext
-    {
+	/// <summary>
+	/// Базовый класс анемичной модели.
+	/// </summary>
+	/// <typeparam name="TBoundedContext">Ограниченный контекст.</typeparam>
+	public abstract class AnemicModel<TBoundedContext> :
+		IAnemicModel<TBoundedContext>
+		where TBoundedContext : IBoundedContext
+	{
 		private Guid _id;
 		protected long _version;
 		private Guid _correlationToken;
@@ -30,29 +30,29 @@ namespace Hive.SeedWorks.TacticalPatterns
 			_correlationToken = correlationToken;
 		}
 
-        /// <summary>
-        /// Имеет идентификатор.
-        /// </summary>
-        public Guid Id => _id;
+		/// <summary>
+		/// Имеет идентификатор.
+		/// </summary>
+		public Guid Id => _id;
 
-        /// <summary>
-        /// Определяет версию. Ожидаемое использование - дата создания версии в милисекундах.
-        /// Является приведением <see cref="DateTimeOffset"/> к формату времени
-        /// Unix в милисекундах.
-        /// </summary>
-        public long Version => _version;
+		/// <summary>
+		/// Определяет версию. Ожидаемое использование - дата создания версии в милисекундах.
+		/// Является приведением <see cref="DateTimeOffset"/> к формату времени
+		/// Unix в милисекундах.
+		/// </summary>
+		public long Version => _version;
 
-        /// <summary>
-        /// Идентификатор комманды, создавшей новую версию.
-        /// </summary>
-        public Guid CorrelationToken => _correlationToken;
+		/// <summary>
+		/// Идентификатор комманды, создавшей новую версию.
+		/// </summary>
+		public Guid CorrelationToken => _correlationToken;
 
-        /// <summary>
-        /// Словарь объект значений.
-        /// </summary>
-        public IDictionary<string, IValueObject> Invariants => GetInvariants();
+		/// <summary>
+		/// Словарь объект значений.
+		/// </summary>
+		public IDictionary<string, IValueObject> Invariants => GetInvariants();
 
-        private IDictionary<string, IValueObject> GetInvariants()
+		private IDictionary<string, IValueObject> GetInvariants()
 		{
 			return _invariants ?? (_invariants = this.GetFields());
 		}
